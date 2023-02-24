44bfbfe1941483e726d6939a32d799d9433761cf
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-02-24 14:46:16 +0100

    a

--- src/components/Work.js
+++ src/components/Work.js
@@ -45,7 +45,7 @@ const Work = () => {
 
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
                 <span className="text-3xl text-gradient cursor-pointer">
-                  <a href="https://analyatech.fr/" target="_blank">
+                  <a href="https://vamb33gcs-chatbot.vercel.app/" target="_blank">
                   ChatBot Application 
                   </a>
                 </span>
@@ -73,7 +73,7 @@ const Work = () => {
 
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
                 <span className="text-3xl text-gradient cursor-pointer">
-                  <a href="https://vamb33gcs-chatbot.vercel.app/" target="_blank">
+                  <a href="https://analyatech.fr/" target="_blank">
                   Ecommerce website
                   </a>
                 </span>
