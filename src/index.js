d00c4673a642349b27ffd1c176705c5fcc1076b1
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-03-03 22:22:42 +0100

    toast

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ToastContainer
+          theme="light"
+          position='top-right'
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />        
     <App />
   </React.StrictMode>
 );
